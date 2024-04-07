e5d1767762b3507fe4c0f6178919144e2a534c09
unknown <akshat.dhiman15@gmail.com>
2024-04-07 15:40:43 +0530

    update: imported dog pkg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Akshat120/puppy
 
 go 1.22.2
+
+require github.com/Akshat120/dog v0.0.0-20240407100738-9e2011bb68b0
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/Akshat120/dog"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
